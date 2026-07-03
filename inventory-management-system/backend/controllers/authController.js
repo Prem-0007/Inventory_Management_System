c70fea42c52983eeef66a988b95b3d904b3a8ca6
Prem <premkumarballa777@gmail.com>
2026-07-03 20:03:40 +0530

    Remove register and login functions from authController
    Removed registration and login functions from authController.

--- inventory-management-system/backend/controllers/authController.js
+++ inventory-management-system/backend/controllers/authController.js
@@ -1,58 +1,5 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE || '7d'
-  });
-};
-
-const register = async (req, res) => {
-  const { name, email, password, role } = req.body;
-
-  if (!name || !email || !password) {
-    return res.status(400).json({ message: 'Please provide name, email and password' });
-  }
-
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    return res.status(400).json({ message: 'User already exists' });
-  }
-
-  const user = await User.create({ name, email, password, role });
-
-  res.status(201).json({
-    _id: user._id,
-    name: user.name,
-    email: user.email,
-    role: user.role,
-    token: generateToken(user._id)
-  });
-};
-
-const login = async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({ message: 'Please provide email and password' });
-  }
-
-  const user = await User.findOne({ email });
-  if (!user || !(await user.matchPassword(password))) {
-    return res.status(401).json({ message: 'Invalid email or password' });
-  }
-
-  res.json({
-    _id: user._id,
-    name: user.name,
-    email: user.email,
-    role: user.role,
-    token: generateToken(user._id)
-  });
-};
-
 const getMe = async (req, res) => {
   res.json(req.user);
 };
 
-module.exports = { register, login, getMe };
+module.exports = { getMe };
